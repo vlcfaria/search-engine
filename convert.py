240cf2d26652f3c901711087cdde3e457039e062
Vinicius Faria <artixsproductions@gmail.com>
2025-06-02 17:30:17 -0300

    fix: added space between lines in converter for better ranking

--- convert.py
+++ convert.py
@@ -3,6 +3,6 @@ with open('corpus.jsonl', 'r') as input:
     with open('converted.jsonl', 'w') as output:
         for line in input:
             obj = json.loads(line)
-            content = '\\n'.join([obj["title"], obj["text"], *obj["keywords"]])
+            content = ' \\n '.join([obj["title"], obj["text"], *obj["keywords"]])
             new = {'id': obj["id"], 'contents': content}
             output.write(f'{json.dumps(new)}\n')
