d0f12b88d8430fc960107e4f396c0effeb682eb7
Vinicius Faria <artixsproductions@gmail.com>
2025-06-05 18:11:33 -0300

    feat: added base experiment class for reproducing experiments, added field based indexing

---
+++ Experiment.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import pyterrier as pt
+
+def Experiment():
+    def __init__(self, index_path: str, corpus_path: str = '') -> None:
+        self.name = 'abstract'
+
+        if corpus_path:
+            self.index = build_index(index_path, corpus_path)
+        else:
+            self.index = get_index(index_path)
+
+        self.search_pipeline = None
+        
+    def get_index(self, index_path):
+        'Gets the given index from `index_path`'
+        raise NotImplementedError()
+
+    def build_index(self, index_path, corpus_path):
+        'Builds the given index in `index_path`, using the given corpus'
+        raise NotImplementedError()
+    
+    def benchmark(self, topics_path: str, qrels_path: str, out_dir: str = ''):
+        'Benchmarks the established search pipeline, according to established metrics'
+
+        qrels = pd.read_csv(qrels_path, 
+                            dtype={'qid': 'object', 'docno': 'object', 'label': 'int64'})
+        topics = pd.read_csv(topics_path, 
+                             dtype={'qid': 'object', 'query': 'object'})
+        
+        ans = pt.Experiment([self.search_pipeline], topics, qrels, 
+                            eval_metrics=['recip_rank', 'ndcg_cut', 'recall'], 
+                            names=[self.name])
+        
+        if out_dir:
+            ans.to_csv(f'{out_dir}/{self.name}-benchmark.csv')
+        else:
+            print(ans)
+
+    def results_tests(self, test_query_path: str, out_dir: str):
+        'Outputs the query results (up to 100) for each test query, using the search pipeline'
+
+        #Load queries
+        queries = pd.read_csv(test_query_path)
+
+        #Transform applies the search
+        df_ans = self.search_pipeline.transform(queries)
+
+        #Detailed result
+        df_ans.to_csv(f'{out_dir}/{self.name}-test-results.csv', index=False)
+
+        #Formatted result, for submitting to kaggle
+        ( df_ans[['qid', 'docno']]
+            .rename(columns={'qid': 'QueryId', 'docno': 'EntityId'})
+            .to_csv(f'{out_dir}/{self.name}-submit-results.csv', index=False)
+        )
--- index_pyterrier.py
+++ index_pyterrier.py
@@ -9,6 +9,7 @@ def parse_args():
 
     parser.add_argument('-i', type=str, help='.jsonl to be used as input', required=True)
     parser.add_argument('-o', type=str, help='output directory', required=True)
+    parser.add_argument('-f', help='index fields separately (title, text, keywords)', action='store_true')
 
     args = parser.parse_args()
 
@@ -19,16 +20,36 @@ def parse_args():
     
     return args
 
-def iter_jsonl(filename):
+def iter_jsonl(filename: str, transform):
+    'Index an entity jsonl file, calling `transform` for every loaded json entity'
+
     with open(filename, 'rt') as file:
         for l in file:
             raw = json.loads(l)
 
-            #Rewrite the raw document
-            text = ' \n '.join([raw['title'], raw['text'], ' \n '.join(raw['keywords'])])
-            yield {'docno': raw['id'], 'text': text}
-    
+            yield transform(raw)
+
+def transform_raw(raw: dict[str, str]) -> dict[str,str]:
+    'Transform an entity dictionary into another entity with a single text field'
+
+    text = ' \n '.join([raw['title'], raw['text'], ' \n '.join(raw['keywords'])])
+    return {'docno': raw['id'], 'text': text}
+
+def transform_fields(raw: dict[str, str]) -> dict[str,str]:
+    'Transform an entity dictionary into an entity with multiple fields'
+
+    return {'docno': raw['id'],
+            'title': raw['title'],
+            'text': raw['text'], 
+            'keywords': ' \n '.join(raw['keywords'])}
 
 if __name__ == '__main__':
     args = parse_args()
-    index = pt.IterDictIndexer(args.o, meta={'docno': 20}, threads=8).index(iter_jsonl(args.i))
+    if not args.f: #Index raw data
+        (pt.IterDictIndexer(args.o, meta={'docno': 20}, blocks=True, threads=8)
+         .index(iter_jsonl(args.i, transform_raw)))
+    else: #Also index fields
+        (pt.IterDictIndexer(args.o, meta={'docno': 20}, 
+                            text_attrs=['title', 'text', 'keywords'], 
+                            fields=True, blocks=True, threads=8)
+         .index(iter_jsonl(args.i, transform_fields)))
