f3636aec7d1a0196e31dda0aef4619d4e4cf1310
Vinicius Faria <artixsproductions@gmail.com>
2025-06-05 21:16:55 -0300

    feat: added handler, removed even more useless files

--- convert.py
+++
@@ -1,8 +0,0 @@
-import json
-with open('corpus.jsonl', 'r') as input:
-    with open('converted.jsonl', 'w') as output:
-        for line in input:
-            obj = json.loads(line)
-            content = ' \\n '.join([obj["title"], obj["text"], *obj["keywords"]])
-            new = {'id': obj["id"], 'contents': content}
-            output.write(f'{json.dumps(new)}\n')
